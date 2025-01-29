f339de5384c52ea9a7bbde1f9198b3fdc279ac97
MuadhAli <mkhanmanzil@gmail.com>
2025-01-29 12:52:20 +0530

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = async (env, options) => {
     devtool: "source-map",
     entry: {
       polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      taskpane: ["./src/taskpane/taskpane.js", "./src/taskpane/taskpane.html"],
+      taskpane: ["./src/taskpane/taskpane.js", "./src/taskpane/index.html"],
       commands: "./src/commands/commands.js",
     },
     output: {
@@ -52,8 +52,8 @@ module.exports = async (env, options) => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        filename: "taskpane.html",
-        template: "./src/taskpane/taskpane.html",
+        filename: "index.html",
+        template: "./src/taskpane/index.html",
         chunks: ["polyfill", "taskpane"],
       }),
       new CopyWebpackPlugin({
